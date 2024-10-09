4cd6ba0d1ec328279ed6ec833d8236b2d14eecc5
CihanBarisTurgut <cihanbaristurgut@gmail.com>
2024-10-09 16:11:12 +0300

    Init

---
+++ src/test/java/tr/edu/ogu/ceng/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.ogu.ceng.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
