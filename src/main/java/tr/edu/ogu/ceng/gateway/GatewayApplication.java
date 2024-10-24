8b9ceefdf4578e3418ad43aba674183e61e852dc
CihanBarisTurgut <cihanbaristurgut@gmail.com>
2024-10-24 13:51:20 +0300

    application.properties were converted to application.yml

--- src/main/java/tr/edu/ogu/ceng/gateway/GatewayApplication.java
+++ src/main/java/tr/edu/ogu/ceng/gateway/GatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.Getter;
 import lombok.Setter;
+import tr.edu.ogu.ceng.gateway.entity.Log;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -12,6 +13,8 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 		System.out.println("görev başarılı");
+		Log log=new Log();
+		System.out.print(log.getId());
 	}
 
 }
