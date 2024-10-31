02364eee8ce9c804aff7613eb5c601ca0b729d2b
berivankorlaelci <medusaiksiri@gmail.com>
2024-10-31 12:11:47 +0300

    added yml ports numbers

--- src/main/java/tr/edu/ogu/ceng/gateway/entity/Refund.java
+++ src/main/java/tr/edu/ogu/ceng/gateway/entity/Refund.java
@@ -4,12 +4,17 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter @Setter
 public class Refund {
 	  	@Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
